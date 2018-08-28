284df5829c2ca720563ef9ba6738911b767f3faf
JR <39860074+dsgrtts@users.noreply.github.com>
2018-08-27 21:02:51 -0400

    Add files via upload

--- work/clearconnector/demo/assets/scripts/scroll.js
+++ work/clearconnector/demo/assets/scripts/scroll.js
@@ -1,1 +1,41 @@
+// scroll.js by @bdc
 
+document.addEventListener("DOMContentLoaded", function () {
+  'use strict';
+  
+  var i, links, root, easeInOutCubic;
+
+  links = document.querySelectorAll('a.scroll');
+  i = links.length;
+  root = /firefox|trident/i.test(navigator.userAgent) ? document.documentElement : document.body;
+  easeInOutCubic = function (t, b, c, d) {
+    if ((t /= d / 2) < 1) {
+      return c / 2 * t * t * t + b;
+    } else {
+      return c / 2 * ((t -= 2) * t * t + 2) + b;
+    }
+  };
+
+  while (i >= 0) {
+    i -= 1;
+    links.item(i).addEventListener('click', function (e) {
+      var startTime, startPos, endPos, maxScroll, scrollEndValue, duration, scroll;
+      
+      startPos = root.scrollTop;
+      endPos = document.getElementById(/[^#]+$/.exec(this.href)[0]).getBoundingClientRect().top;
+      maxScroll = root.scrollHeight - window.innerHeight;
+      scrollEndValue = startPos + endPos < maxScroll ? endPos : maxScroll - startPos;
+      duration = 900;
+      scroll = function (timestamp) {
+        startTime = startTime || timestamp;
+        var elapsed, progress;
+        elapsed = timestamp - startTime;
+        progress = easeInOutCubic(elapsed, startPos, scrollEndValue, duration);
+        root.scrollTop = progress;
+        elapsed < duration && requestAnimationFrame(scroll);
+      };
+      requestAnimationFrame(scroll);
+      e.preventDefault();
+    });
+  }
+});
